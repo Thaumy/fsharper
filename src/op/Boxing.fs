349a1cca833738f51e22f56c2910f4ed09707474
Thaumy <Studio_ai@outlook.com>
2022-05-22 23:45:53 +0800

    helpers and <. operator

--- src/op/Boxing.fs
+++ src/op/Boxing.fs
@@ -9,6 +9,12 @@ let inline unwrap m = (^m: (member unwrap : unit -> ^v) m)
 let inline unwrap2 m = m |> unwrap |> unwrap
 let inline unwrap3 m = m |> unwrap2 |> unwrap
 
+let inline whenCanUnwrap m f =
+    (^m: (member whenCanUnwrap : 'v -> unit) m, f)
+
+let inline whenCanUnwrapOr m trueDo falseDo =
+    (^m: (member whenCanUnwrapOr : ('v -> 'r) * (unit -> 'r) -> 'r) m, trueDo, falseDo)
+
 let inline unwrapOr m value =
     (^m: (member unwrapOr : ^v -> ^v) m, value)
 
