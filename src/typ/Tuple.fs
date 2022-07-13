1918e63250e68ae53797e6f67af3c6c0fe621c6d
Thaumy <thaumy@outlook.com>
2022-07-13 11:29:56 +0800

    tuple ext

--- src/typ/Tuple.fs
+++ src/typ/Tuple.fs
@@ -1,6 +1,8 @@
 ﻿[<AutoOpen>]
 module fsharper.typ.Tuple
 
+open System.Runtime.CompilerServices
+
 [<AutoOpen>]
 module fn =
 
@@ -9,3 +11,11 @@ module fn =
     let inline snd3 (_, x, _) = x
 
     let inline trd3 (_, _, x) = x
+
+[<Extension>]
+type ext() =
+    [<Extension>]
+    static member inline fst((x, _)) = x
+
+    [<Extension>]
+    static member inline snd((_, y)) = y
