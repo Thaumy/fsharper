0df9c2dda98a531fa6945d7b7eb2313c1479f52f
Thaumy <thaumy@outlook.com>
2022-09-18 15:04:59 +0800

    Update Coerce.fs

--- src/op/Coerce.fs
+++ src/op/Coerce.fs
@@ -3,14 +3,17 @@ module fsharper.op.Coerce
 
 open System
 
-let inline is<'b> a = typeof<'b>.IsInstanceOfType a
+let is<'b> a = typeof<'b>.IsInstanceOfType a
 
 //when 'a = 'b, coerce is eq to id
 let inline coerce (a: 'a) : 'b =
     downcast Convert.ChangeType(a, typeof<'b>)
 
+let inline cast (a: 'a) : 'b = downcast (a :> obj)
+
 type Object with
 
     member self.is<'b>() = is<'b> self
-
     member self.coerce() = self |> coerce
+    member self.cast() = self |> cast
+    member self.obj() = self
